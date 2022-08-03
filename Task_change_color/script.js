1942194de9164eddda186af26a8625d72c32e78a
grena1216 <grenapanchal012@gmail.com>
2022-08-03 17:17:54 +0530

    add files

--- Task_change_color/script.js
+++ Task_change_color/script.js
@@ -4,3 +4,11 @@ function change(){
     var x=Math.floor(Math.random() * 17)
     document.body.style.backgroundColor = color[x];
 }
+// Automatic color change after click a button one time
+function auto(){
+    window.setTimeout( "auto()", 1000);
+    var color = ["aquamarine","antiquewhite","lightgreen","beige","lightblue","lightgray","lightyellow","darkslategrey","darkcyan","crimson",
+    "darkolivegreen","darkred","violate","burlywood","chocolate","hotpick","darkorchid","lightcoral"]
+var x=Math.floor(Math.random() * 17)
+document.body.style.backgroundColor = color[x];
+}
